a627aafe79f88c11e0b7c22944cc4bd7bcdbfba3
tomieric <tomieric@gmail.com>
2014-08-20 11:44:47 +0800

    'update'

---
+++ configs.js
@@ -0,0 +1,22 @@
+var airpubConfigs = {
+  // blog name
+  name: '前端 - 分享园',
+  // just a little description
+  description: '前端|css|html5|css3|jquery|javascript|nodejs|angular|angularjs|backbone|移动开发',
+  // blog uri
+  url: 'http://getf2e.com',
+  // blog theme
+  // theme: 'my-theme',
+  // for images upload
+  upyun: {
+    bucket: 'getf2e',
+    uri: 'http://getf2e.b0.upaiyun.com',
+    endpoint: 'http://v0.api.upyun.com/getf2e',
+    form_api_secret: 'FIwZlbr8pEG3Qc8Fl3ZusdxG+aQ='
+  }
+}
+
+// for backend service
+var duoshuoQuery = {
+  short_name: 'getf2e'
+};
